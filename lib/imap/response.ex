70e88fd89e1adf1e016c907674083d5c632e1e50
Po Chen <chenpaul914@gmail.com>
2020-02-06 15:39:59 +1100

    add simple parsing for select/examine/fetch

--- lib/imap/response.ex
+++ lib/imap/response.ex
@@ -2,48 +2,77 @@ defmodule Imap.Response do
   @moduledoc ~S"""
   Parse responses returned by the IMAP server and convert them to a structured format
   """
-  defstruct request: nil, body: [], status: "OK", error: nil, message: nil, partial: false
 
-#  @response_codes [
-#    "ALERT", "BADCHARSET", "CAPABILITY", "PARSE", "PERMANENTFLAGS", "READ-ONLY", "READ-WRITE",
-#    "TRYCREATE", "UIDNEXT", "UIDVALIDITY", "UNSEEN"
-#  ]
-
-  def parse(resp, line, rest) do
-    parse_tag(resp, line, rest)
+  def parse(type, {%Imap.Client{}, response}) do
+    parse(type, response)
   end
 
-  defp parse_tag(resp, line, rest) do
-    tag = resp.request.tag
-    tag_size = byte_size(tag)
-    case line do
-      "* " <> message ->
-        parse_message(:untagged, resp, message, rest)
-      <<^tag::bytes-size(tag_size)>> <> " " <> message ->
-        parse_message(:tagged, resp, message, rest)
-    end
+  def parse(type, {:ok, [response: response], "", _, _, _}) do
+    parse(type, response)
   end
 
-  defp parse_message(:untagged, resp, msg, rest) do
-    # handle one term msg, eg Request.search(["ALL"]) on an empty mailbox returns "* SEARCH\r\n"
-    {type, msg} =
-      case String.split(msg, " ", parts: 2) do
-        [type, msg] -> {type, msg}
-        [type] -> {type, nil}
-      end
-    {:ok, append_to_response(resp, item: %{type: type, message: msg}), rest}
+  def parse(type, parts) when type in [:select, :examine] do
+    {:ok, Enum.map(parts, &parse_list_part/1)}
   end
 
-  defp parse_message(:tagged, resp, "NO " <> msg, rest), do: {:ok, append_to_response(resp, status: "NO", partial: false, message: msg), rest}
-  defp parse_message(:tagged, resp, "BAD " <> msg, rest), do: {:ok, append_to_response(resp, status: "BAD", partial: false, message: msg), rest}
-  defp parse_message(:tagged, resp, "OK " <> msg, rest), do: {:ok, append_to_response(resp, status: "OK", partial: false, message: msg), rest}
-  defp parse_message(:tagged, resp, msg, rest), do: {:ok, append_to_response(resp, status: "UNKNOWN", partial: false, message: msg), rest}
+  def parse(
+        :fetch,
+        [
+          {:response_data,
+           [
+             "*",
+             " ",
+             {:message_data,
+              [
+                _index,
+                " ",
+                "FETCH",
+                " ",
+                {:msg_att,
+                 [
+                   "(",
+                   {:msg_att_static, msg_att_static},
+                   ")"
+                 ]}
+              ]}
+             | _
+           ]}
+          | _
+        ]
+      ) do
+    {:ok, msg_att_static |> parse_msg_att_static() |> Mail.Parsers.RFC2822.parse()}
+  end
 
-  defp append_to_response(resp, opts) do
-    status = Keyword.get(opts, :status, "OK")
-    item = Keyword.get(opts, :item, %{})
-    message = Keyword.get(opts, :message, "")
-    partial = Keyword.get(opts, :partial, true)
-    %Imap.Response{resp | body: [item | resp.body], message: message, status: status, partial: partial}
+  defp parse_list_part(
+         {:response_data, ["*", " ", {:mailbox_data, [{:number, number}, " ", property]}, _]}
+       ) do
+    {property, number}
+  end
+
+  defp parse_list_part(
+         {:response_data, ["*", " ", {:mailbox_data, ["FLAGS", " ", {:flag_list, flags}]}, _]}
+       ) do
+    {:flags, Enum.reject(flags, &is_binary/1)}
+  end
+
+  defp parse_list_part(
+         {:response_data, ["*", " ", {:resp_cond_state, ["OK", " ", {:resp_text, data}]}, _]}
+       ) do
+    data
+  end
+
+  defp parse_list_part(
+         {:response_done,
+          [response_tagged: [_tag, " ", {:resp_cond_state, ["OK", " ", {:resp_text, data}]}, _]]}
+       ) do
+    data
+  end
+
+  defp parse_msg_att_static(["RFC822", " ", {:literal, [_number, body]}]) do
+    body
+  end
+
+  defp parse_msg_att_static(["BODY", _section, " ", {:literal, [_number, body]}]) do
+    body
   end
 end
