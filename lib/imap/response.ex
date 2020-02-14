7d475c88e688cd705bf4836319df588bba36b620
Po Chen <chenpaul914@gmail.com>
2020-02-15 09:50:45 +1100

    allow parsing fetch of multiple emails

--- lib/imap/response.ex
+++ lib/imap/response.ex
@@ -12,60 +12,60 @@ defmodule Imap.Response do
   end
 
   def parse(type, parts) when type in [:select, :examine] do
-    {:ok, Enum.map(parts, &parse_list_part/1)}
+    {:ok, Enum.map(parts, &parse_message_data/1)}
   end
 
-  def parse(
-        :fetch,
-        [
-          {:response_data,
-           [
-             "*",
-             " ",
-             {:message_data,
-              [
-                _index,
-                " ",
-                "FETCH",
-                " ",
-                {:msg_att,
-                 [
-                   "(",
-                   {:msg_att_static, msg_att_static},
-                   ")"
-                 ]}
-              ]}
-             | _
-           ]}
-          | _
-        ]
-      ) do
-    {:ok, msg_att_static |> parse_msg_att_static() |> Mail.Parsers.RFC2822.parse()}
+  def parse(:fetch, parts) do
+    {:ok, Enum.map(parts, &parse_message_data/1)}
   end
 
-  defp parse_list_part(
+  defp parse_message_data(
          {:response_data, ["*", " ", {:mailbox_data, [{:number, number}, " ", property]}, _]}
        ) do
     {property, number}
   end
 
-  defp parse_list_part(
+  defp parse_message_data(
          {:response_data, ["*", " ", {:mailbox_data, ["FLAGS", " ", {:flag_list, flags}]}, _]}
        ) do
     {:flags, Enum.reject(flags, &is_binary/1)}
   end
 
-  defp parse_list_part(
+  defp parse_message_data(
          {:response_data, ["*", " ", {:resp_cond_state, ["OK", " ", {:resp_text, data}]}, _]}
        ) do
     data
   end
 
-  defp parse_list_part(
+  defp parse_message_data(
          {:response_done,
           [response_tagged: [_tag, " ", {:resp_cond_state, ["OK", " ", {:resp_text, data}]}, _]]}
        ) do
     data
+  end
+
+  defp parse_message_data(
+         {:response_data,
+          [
+            "*",
+            " ",
+            {:message_data,
+             [
+               _index,
+               " ",
+               "FETCH",
+               " ",
+               {:msg_att,
+                [
+                  "(",
+                  {:msg_att_static, msg_att_static},
+                  ")"
+                ]}
+             ]}
+            | _
+          ]}
+       ) do
+    msg_att_static |> parse_msg_att_static() |> Mail.Parsers.RFC2822.parse()
   end
 
   defp parse_msg_att_static(["RFC822", " ", {:literal, [_number, body]}]) do
